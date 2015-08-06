09bd251f8b2da31c06335ac5df60e02db3fc1e66
unknown <p.a.brown@MW71F33QYULTR1.dir.svc.accenture.com>
2015-08-06 15:28:01 +0200

    controller setup

---
+++ src/main/java/com/accenture/shop/ShopController.java
@@ -0,0 +1,35 @@
+package com.accenture.shop;
+
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class ShopController {
+	
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/books", method = RequestMethod.GET)
+	public String books(Locale locale, Model model) {
+		
+		return "books";
+	}
+	@RequestMapping(value = "/books/cart", method = RequestMethod.GET)
+	public String cart(Locale locale, Model model) {
+		
+		return "cart";
+	}
+	@RequestMapping(value = "/books/pay", method = RequestMethod.GET)
+	public String pay(Locale locale, Model model) {
+		
+		return "cart";
+	}		
+}
